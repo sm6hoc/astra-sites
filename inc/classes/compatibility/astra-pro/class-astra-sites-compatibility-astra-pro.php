a3bfee7f4ff29ad46f830f1fc67a7e343e11c8ee
Mahesh Waghmare <mwaghmare7@gmail.com>
2018-01-22 17:34:51 +0530

    Included post data mapping and nav menu static link process in batch import process.

--- inc/classes/compatibility/astra-pro/class-astra-sites-compatibility-astra-pro.php
+++ inc/classes/compatibility/astra-pro/class-astra-sites-compatibility-astra-pro.php
@@ -32,7 +32,7 @@ if ( ! class_exists( 'Astra_Sites_Compatibility_Astra_Pro' ) ) :
 		 * @since 1.0.0
 		 * @return object initialized object of class.
 		 */
-		public static function instance() {
+		public static function get_instance() {
 			if ( ! isset( self::$instance ) ) {
 				self::$instance = new self;
 			}
@@ -48,6 +48,18 @@ if ( ! class_exists( 'Astra_Sites_Compatibility_Astra_Pro' ) ) :
 			add_action( 'astra_sites_after_plugin_activation', array( $this, 'astra_pro' ), 10, 2 );
 			add_action( 'astra_sites_import_start', array( $this, 'import_enabled_extension' ), 10, 2 );
 			add_action( 'astra_sites_import_start', array( $this, 'import_custom_404' ), 10, 2 );
+		}
+
+		/**
+		 * Import
+		 *
+		 * @since 1.1.6
+		 * @return void
+		 */
+		public function import() {
+			Astra_Sites_Image_Importer::log( '---- Processing Mapping - for Astra Pro ----' );
+
+			self::start_post_mapping();
 		}
 
 		/**
@@ -75,7 +87,6 @@ if ( ! class_exists( 'Astra_Sites_Compatibility_Astra_Pro' ) ) :
 					}
 				}
 			}
-
 		}
 
 		/**
@@ -111,11 +122,170 @@ if ( ! class_exists( 'Astra_Sites_Compatibility_Astra_Pro' ) ) :
 			}
 		}
 
+		/**
+		 * Start post meta mapping of Astra Addon
+		 *
+		 * @since 1.1.6
+		 *
+		 * @return null     If there is no import option data found.
+		 */
+		public static function start_post_mapping() {
+			$demo_data = get_option( 'astra_sites_import_data', array() );
+			if ( ! isset( $demo_data['astra-post-data-mapping'] ) ) {
+				return;
+			}
+
+			$post_type = 'astra-advanced-hook';
+			$posts     = ( isset( $demo_data['astra-post-data-mapping'][ $post_type ] ) ) ? $demo_data['astra-post-data-mapping'][ $post_type ] : array();
+			if ( ! empty( $posts ) ) {
+				foreach ( $posts as $key => $post ) {
+					$page = get_page_by_title( $post['post_title'], OBJECT, $post_type );
+					if ( is_object( $page ) ) {
+						self::update_location_rules( $page->ID, 'ast-advanced-hook-location', $post['mapping']['ast-advanced-hook-location'] );
+					}
+				}
+			}
+
+			$post_type = 'astra_adv_header';
+			$posts     = ( isset( $demo_data['astra-post-data-mapping'][ $post_type ] ) ) ? $demo_data['astra-post-data-mapping'][ $post_type ] : array();
+			if ( ! empty( $posts ) ) {
+				foreach ( $posts as $key => $post ) {
+					$page = get_page_by_title( $post['post_title'], OBJECT, $post_type );
+					if ( is_object( $page ) ) {
+
+						self::update_location_rules( $page->ID, 'ast-advanced-headers-location', $post['mapping']['ast-advanced-headers-location'] );
+						self::update_location_rules( $page->ID, 'ast-advanced-headers-exclusion', $post['mapping']['ast-advanced-headers-exclusion'] );
+						self::update_header_mapping( $page->ID, 'ast-advanced-headers-design', $post['mapping']['ast-advanced-headers-design'] );
+					}
+				}
+			}
+		}
+
+		/**
+		 * Update Header Mapping Data
+		 *
+		 * @since 1.1.6
+		 *
+		 * @param  int    $post_id     Post ID.
+		 * @param  string $meta_key Post meta key.
+		 * @param  array  $mapping  Mapping array.
+		 * @return void
+		 */
+		public static function update_header_mapping( $post_id = '', $meta_key = '', $mapping = array() ) {
+			Astra_Sites_Image_Importer::log( 'Mapping "' . $meta_key . '" for ' . $post_id );
+
+			$headers_old = get_post_meta( $post_id, $meta_key, true );
+			$headers_new = self::get_header_mapping( $headers_old, $mapping );
+			update_post_meta( $post_id, $meta_key, $headers_new );
+		}
+
+		/**
+		 * Update Location Rules
+		 *
+		 * @since 1.1.6
+		 *
+		 * @param  int    $post_id     Post ID.
+		 * @param  string $meta_key Post meta key.
+		 * @param  array  $mapping  Mapping array.
+		 * @return void
+		 */
+		public static function update_location_rules( $post_id = '', $meta_key = '', $mapping = array() ) {
+			Astra_Sites_Image_Importer::log( 'Mapping "' . $meta_key . '" for ' . $post_id );
+
+			$location_new = self::get_location_mappings( $mapping );
+			update_post_meta( $post_id, $meta_key, $location_new );
+		}
+
+		/**
+		 * Get mapping locations.
+		 *
+		 * @since 1.1.6
+		 *
+		 * @param  array $location Location data.
+		 * @return array            Location mapping data.
+		 */
+		public static function get_location_mappings( $location = array() ) {
+			if ( empty( $location ) ) {
+				return $location;
+			}
+
+			if ( ! isset( $location['specific'] ) || empty( $location['specific'] ) ) {
+				return $location;
+			}
+
+			$mapping = array();
+
+			if ( isset( $location['specific']['post'] ) ) {
+				foreach ( $location['specific']['post'] as $post_type => $old_post_data ) {
+					if ( is_array( $old_post_data ) ) {
+						foreach ( $old_post_data as $post_key => $post ) {
+							if ( $post_object = get_page_by_path( $post['slug'] ) ) {
+								$mapping[] = 'post-' . absint( $post_object->ID );
+							}
+						}
+					}
+				}
+			}
+
+			if ( isset( $location['specific']['tax'] ) ) {
+				foreach ( $location['specific']['tax'] as $taxonomy_type => $old_term_data ) {
+					if ( is_array( $old_term_data ) ) {
+						foreach ( $old_term_data as $term_key => $term_data ) {
+							$term = get_term_by( 'slug', $term_data['slug'], $taxonomy_type );
+							if ( is_object( $term ) ) {
+								$mapping[] = 'tax-' . absint( $term->term_id );
+							}
+						}
+					}
+				}
+			}
+
+			$location['specific'] = $mapping;
+
+			return $location;
+		}
+
+		/**
+		 * Get advanced header mapping data
+		 *
+		 * @since 1.1.6
+		 *
+		 * @param  array $headers_old  Header mapping stored data.
+		 * @param  array $headers_data Header mapping data.
+		 * @return array                Filtered header mapping data.
+		 */
+		public static function get_header_mapping( $headers_old = array(), $headers_data = array() ) {
+
+			// Set menu location by menu slug.
+			if ( isset( $headers_data['menus'] ) && ! empty( $headers_data['menus'] ) ) {
+				foreach ( $headers_data['menus'] as $header_option_name => $menu_data ) {
+					$term = get_term_by( 'slug', $menu_data['slug'], 'nav_menu' );
+					if ( is_object( $term ) ) {
+						$headers_old[ $header_option_name ] = $term->term_id;
+					}
+				}
+			}
+
+			// Set image ID & URL after importing these on website.
+			if ( isset( $headers_data['images'] ) && ! empty( $headers_data['images'] ) ) {
+				foreach ( $headers_data['images'] as $key => $image_data ) {
+					if ( isset( $image_data['image'] ) && ! empty( $image_data['image'] ) ) {
+						$downloaded_image = Astra_Sites_Image_Importer::get_instance()->import( $image_data['image'] );
+
+						$headers_old[ $image_data['key_map']['url'] ] = $downloaded_image['url'];
+						$headers_old[ $image_data['key_map']['id'] ]  = $downloaded_image['id'];
+					}
+				}
+			}
+
+			return $headers_old;
+		}
+
 	}
 
 	/**
-	 * Kicking this off by calling 'instance()' method
+	 * Kicking this off by calling 'get_instance()' method
 	 */
-	Astra_Sites_Compatibility_Astra_Pro::instance();
+	Astra_Sites_Compatibility_Astra_Pro::get_instance();
 
 endif;
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://wpastra.com/pro/
 Tags: demo, theme demos, one click import
 Requires at least: 4.4
 Tested up to: 4.9.1
-Stable tag: 1.1.5
+Stable tag: 1.1.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -55,6 +55,12 @@ https://wpastra.com/sites-suggestions/
 3. Import the demo.
 
 == Changelog ==
+
+v1.1.6 - 22-January-2018
+* New: Added filter `astra_sites_xml_import_options` to change the XML import options.
+* Fix: Astra Pro plugin 'Custom Layouts' & 'Page Headers' not setting right display location due to different page, tax, category ids.
+* Fix: WooCommerce shop, checkout cart page ids not setting issue.
+* Fix: After site import updated demo url from the nav menus.
 
 v1.1.5 - 11-January-2018
 * New: Added SVG file support for importing the SVG images.
