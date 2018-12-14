1ef03ada89b2d86c1c07e90a415fe5a51d2028fc
Mahesh Waghmare <mwaghmare7@gmail.com>
2018-12-14 16:12:09 +0530

    Updated notice string.

--- inc/classes/class-astra-sites-importer.php
+++ inc/classes/class-astra-sites-importer.php
@@ -147,7 +147,7 @@ if ( ! class_exists( 'Astra_Sites_Importer' ) ) :
 			do_action( 'astra_sites_import_prepare_xml_data' );
 
 			if ( ! class_exists( 'XMLReader' ) ) {
-				wp_send_json_error( __( 'The XMLReader is not enabled on your server. To enable it please contact your server administrator.', 'astra-sites' ) );
+				wp_send_json_error( __( 'If XMLReader is not available, it imports all other settings and only skips XML import. This creates an incomplete website. We should bail early and not import anything if this is not present.', 'astra-sites' ) );
 			}
 
 			$wxr_url = ( isset( $_REQUEST['wxr_url'] ) ) ? urldecode( $_REQUEST['wxr_url'] ) : '';
