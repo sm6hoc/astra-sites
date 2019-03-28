910f37de7f93282ad18c892974f01a1feeb80dda
Mahesh Waghmare <maheshw@bsf.io>
2019-03-28 13:20:25 +0530

    Improvements.

--- inc/assets/js/astra-sites-api.js
+++ inc/assets/js/astra-sites-api.js
@@ -16,16 +16,14 @@
 
 			fetch( AstraSitesAPI._api_url + args.slug, {
 	            method: 'GET',
-	            localCache: true,
-	            cacheKey: 'AstraJsonCache-' + args.slug,
-	            cache: "force-cache",
+	            cache: 'force-cache',
 	        }).then(response => {
 				if ( response.status === 200 ) {
 		        	return response.json().then(items => ({
 						args 		: args,
 						items 		: items,
-						items_count	: response.headers.get("x-wp-total"),
-						item_pages	: response.headers.get("x-wp-totalpages"),
+						items_count	: response.headers.get('x-wp-total'),
+						item_pages	: response.headers.get('x-wp-totalpages'),
 					}))
 				} else {
 					$(document).trigger( 'astra-sites-api-request-error' );
@@ -33,68 +31,12 @@
 				}
 	        })
 			.then(data => {
-
-				console.log( data );
-
 				if( 'undefined' !== data && '' !== data ) {
-					console.log( args.trigger );
-					console.log( '------------------------' );
-					console.log( data );
-
 					if( 'undefined' !== args.trigger && '' !== args.trigger ) {
 						$(document).trigger( args.trigger, [data] );
 					}
 				}
 	        });
-
-
-			/*// Set API Request Data.
-			var data = {
-				url: AstraSitesAPI._api_url + args.slug,
-			};
-
-			if( astraRenderGrid.headers ) {
-				data.headers = astraRenderGrid.headers;
-			}
-
-			$.ajax( data )
-			.done(function( items, status, XHR ) {
-
-				if( 'success' === status && XHR.getResponseHeader('x-wp-total') ) {
-
-					if( args.id ) {
-						AstraSitesAPI._stored_data[ args.id ] = $.merge( AstraSitesAPI._stored_data[ args.id ], items );
-					}
-
-					var data = {
-						args 		: args,
-						items 		: items,
-						items_count	: XHR.getResponseHeader('x-wp-total') || 0,
-					};
-
-					console.log( args.trigger );
-					console.log( '------------------------' );
-					console.log( data );
-
-					if( 'undefined' !== args.trigger && '' !== args.trigger ) {
-						$(document).trigger( args.trigger, [data] );
-					}
-
-				} else {
-					$(document).trigger( 'astra-sites-api-request-error' );
-				}
-
-			})
-			.fail(function( jqXHR, textStatus ) {
-
-				$(document).trigger( 'astra-sites-api-request-fail', [jqXHR, textStatus, args] );
-
-			})
-			.always(function() {
-
-				$(document).trigger( 'astra-sites-api-request-always' );
-
-			});*/
 
 		},
 
